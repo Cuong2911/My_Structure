f05a0a8b8ce11d01010d4c2cded2bf659bc367db
Cuong2911 <hvcuong29112001@gmail.com>
2024-03-10 18:08:06 +0700

    update react bootstrap

--- src/Pages/Page1/index.tsx
+++ src/Pages/Page1/index.tsx
@@ -1,15 +1,25 @@
+import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const index = () => {
     return (
         <div>
             <h1>Page 1</h1>
-            <button>
-                <NavLink to="/Page2">Page 2</NavLink>
-            </button>
+            <Button variant="primary">
+                <NavLink to="/Page2" className="text-white">
+                    Page 2
+                </NavLink>
+            </Button>{' '}
             <button>
                 <NavLink to="/Page3">Page 3</NavLink>
             </button>
+            <div>
+                <DropdownButton variant="success" title="Explore" size="lg">
+                    <Dropdown.Item> Courses </Dropdown.Item>
+                    <Dropdown.Item> Practice </Dropdown.Item>
+                    <Dropdown.Item> Offers </Dropdown.Item>
+                </DropdownButton>
+            </div>
         </div>
     );
 };
